74e4524f81ce670156344bbdb632591ede521421
Erik Krieg <erik.c.krieg@gmail.com>
2017-07-16 19:44:08 -0400

    updates resolvers to use mongo connector

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,25 +1,13 @@
-const links = [
-    {
-        id: 1,
-        url: 'http://graphql.org/',
-        description: 'The Best Query Language'
-    },
-    {
-        id: 2,
-        url: 'http://dev.apollodata.com',
-        description: 'Awesome GraphQL Client'
-    }
-]
-
 module.exports = {
     Query: {
-        allLinks: () => links
+        allLinks: (root, data, { mongo }) => {
+            return mongo.Links.find({}).toArray()
+        }
     },
     Mutation: {
-        createLink: (_, data) => {
-            const newLink = Object.assign({ id: links.length + 1 }, data)
-            links.push(newLink)
-            return newLink
+        createLink: async (root, data, { mongo }) => {
+            const response = await mongo.Links.insert(data)
+            return Object.assign({ id: response.insertedIds[0] }, data)
         }
     }
 }
