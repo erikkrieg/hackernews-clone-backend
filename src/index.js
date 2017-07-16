a55500aa9b5e8b27cd433cdf54f353d99518aa73
Erik Krieg <erik.c.krieg@gmail.com>
2017-07-16 18:49:02 -0400

    adds graphiql route to server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { graphqlExpress } = require('graphql-server-express')
+const { graphqlExpress, graphiqlExpress } = require('graphql-server-express')
 
 const schema = require('./schema')
 
 const PORT = 3000
+const endpointURL = '/graphql'
 const app = express()
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
+app.use(endpointURL, bodyParser.json(), graphqlExpress({schema}))
+app.use('/graphiql', graphiqlExpress({ endpointURL }))
 
 app.listen(PORT, () => {
     console.log(`Hackernews GraphQL server running on port ${PORT}.`)
