bf3d0f047a6ab1523f53e0b2fc094e4a402944bb
Erik Krieg <erik.c.krieg@gmail.com>
2017-07-16 18:19:20 -0400

    adds schema for Link type

---
+++ src/schema/index.js
@@ -0,0 +1,11 @@
+const { makeExecutableSchema } = require('graphql-tools');
+
+const typeDefs = `
+    type Link {
+        id: ID!
+        url: String!
+        description: String!
+    }
+`;
+
+module.exports = makeExecutableSchema(typeDefs);
