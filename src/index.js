bdd3c59da2026d56df091825b9a17c3f4b86f3c9
Erik Krieg <erik.c.krieg@gmail.com>
2017-07-16 19:37:41 -0400

    updates server to pass mongo connector to gql context

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ const bodyParser = require('body-parser')
 const { graphqlExpress, graphiqlExpress } = require('graphql-server-express')
 
 const schema = require('./schema')
+const connectMongo = require('./mongo-connector')
 
 const PORT = 3000
 const endpointURL = '/graphql'
-const app = express()
 
-app.use(endpointURL, bodyParser.json(), graphqlExpress({schema}))
-app.use('/graphiql', graphiqlExpress({ endpointURL }))
+async function start () {
+    const mongo = await connectMongo()
+    const app = express()
 
-app.listen(PORT, () => {
-    console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-})
+    app.use(endpointURL, bodyParser.json(), graphqlExpress({
+        schema,
+        context: { mongo }
+    }))
+    app.use('/graphiql', graphiqlExpress({ endpointURL }))
+
+    app.listen(PORT, () => {
+        console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+    })
+}
+
+start()
